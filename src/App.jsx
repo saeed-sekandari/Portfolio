5eda5ffe0440cca24e67a4ea6be6b7a70b4ce40e
Saeed Sekandari <sekandari2003@gmail.com>
2026-08-03 19:01:22 -0700

    Initial portfolio setup and Added Navbar

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <header>
+        <h1>Saeed Sekandari</h1>
+        <p>Software Engineering Student</p>
+      </header>
+
+      <main>
+        <section>
+          <h2>About Me</h2>
+          <p>
+            Welcome to my portfolio! I'm a Software Engineering student passionate
+            about building Java applications and learning modern web development.
+          </p>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App;
